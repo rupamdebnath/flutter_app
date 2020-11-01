8db91dd6434c2f40af2c5c2ab8ab41c299bb8579
rupamdebnath <rdebmail@gmail.com>
2020-11-01 12:40:06 +1100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(
+    new MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+              child: Text('I am Rich')),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+      )
+    )
+  );
+}
